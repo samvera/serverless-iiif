dd2657a846a526ab7b3bfc5f45d581cae3b55c2f
Trey Pendragon <tpendragon@princeton.edu>
2020-03-30 10:23:32 -0700

    Add deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,12 @@ if [ $1 == "production" ]
 then
   sam deploy --stack-name=iiif-serverless-production \
   --s3-prefix=iiif-serverless-production \
-  --parameter-overrides='StageName="production", SourceBucket="iiif-image-production", Alias="iiif-cloud.princeton.edu"' \
-  --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
-  --region='us-east-1' \
-  --capabilities='CAPABILITY_IAM' \
-  --profile='figgy-deploy'
-elif [ $1 == "staging" ]
+  --parameter-overrides='StageName="production", SourceBucket="iiif-image-staging"'
+elif [$1 == "staging" ]
 then
   sam deploy --stack-name=iiif-serverless-staging \
   --s3-prefix=iiif-serverless-staging \
-  --parameter-overrides='StageName="staging", SourceBucket="iiif-image-staging", Alias="iiif-cloud-staging.princeton.edu"' \
-  --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
-  --region='us-east-1' \
-  --capabilities='CAPABILITY_IAM' \
-  --profile='figgy-deploy'
+  --parameter-overrides='StageName="staging", SourceBucket="iiif-image-staging"'
 else
   echo 'Please enter either production or staging as the environment'
 fi
